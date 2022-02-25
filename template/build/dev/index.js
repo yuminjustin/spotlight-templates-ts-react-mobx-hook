55f03850ab123e58a8f98f29699b44f87ef21485
justin lee <yuminjustin@163.com>
2022-02-25 15:09:37 +0800

    Update index.js

--- template/build/dev/index.js
+++ template/build/dev/index.js
@@ -7,7 +7,7 @@ let _dev = config.dev;
 console.log(chalk.green('> 正在启动web服务\n' + '> 监听端口:' + _dev.port + '\n'));
 console.log(chalk.cyan('开发地址:http://'+_dev.host+':' + _dev.port + '\n'));
 
-shell.exec('cross-env NODE_ENV=development webpack server --open --progress --config  build/dev/dev.js', (code, stdout, stderr) => {
+shell.exec('cross-env NODE_ENV=development webpack server --open --progress --config  build/dev/dev.js', function (code, stdout, stderr) {
     if (code == 0) {
         console.log(chalk.green(stdout))
     } else {
