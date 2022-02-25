20fc1d8d8373d2281936ccecd22d1c85e92cd524
justin lee <yuminjustin@163.com>
2022-02-25 15:10:25 +0800

    Update base.js

--- template/build/common/base.js
+++ template/build/common/base.js
@@ -23,38 +23,38 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.(ts|tsx)$/,
-            loader: 'ts-loader',
-            include: [codeBase]
-        }, {
-            test: /\.(js|jsx)$/,
-            loader: 'babel-loader',
-            include: [codeBase]
-        },
-        {
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: path.posix.join(_dir, 'image/[name].[hash:5].[ext]') //image 文件夹
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader',
+                include: [codeBase]
+            }, {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                include: [codeBase]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: path.posix.join(_dir, 'image/[name].[hash:5].[ext]') //image 文件夹
+                }
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: path.posix.join(_dir, 'media/[name].[hash:5].[ext]') //media 文件夹
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: path.posix.join(_dir, 'fonts/[name].[hash:5].[ext]') //fonts 文件夹
+                }
             }
-        },
-        {
-            test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: path.posix.join(_dir, 'media/[name].[hash:5].[ext]') //media 文件夹
-            }
-        },
-        {
-            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: path.posix.join(_dir, 'fonts/[name].[hash:5].[ext]') //fonts 文件夹
-            }
-        }
         ]
     }
 }
